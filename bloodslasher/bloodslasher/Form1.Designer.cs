eac1468b616e85624fb7bb794e65ee32df953b51
N e o <paul.gardien@epitech.eu>
2020-07-31 18:50:50 +0200

    Add files via upload

---
+++ bloodslasher/bloodslasher/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace bloodslasher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inject = new System.Windows.Forms.Button();
+            this.cookie = new System.Windows.Forms.TextBox();
+            this.maximum = new System.Windows.Forms.TextBox();
+            this.bloodpoint = new System.Windows.Forms.Label();
+            this.allbloodpoint = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "made by Neo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(225, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bhvr cookie:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(186, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Bloodpoints to inject:";
+            // 
+            // inject
+            // 
+            this.inject.BackColor = System.Drawing.Color.Transparent;
+            this.inject.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inject.ForeColor = System.Drawing.SystemColors.Window;
+            this.inject.Location = new System.Drawing.Point(413, 187);
+            this.inject.Name = "inject";
+            this.inject.Size = new System.Drawing.Size(75, 23);
+            this.inject.TabIndex = 8;
+            this.inject.Text = "Inject";
+            this.inject.UseVisualStyleBackColor = false;
+            this.inject.Click += new System.EventHandler(this.inject_Click);
+            // 
+            // cookie
+            // 
+            this.cookie.BackColor = System.Drawing.SystemColors.WindowText;
+            this.cookie.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cookie.ForeColor = System.Drawing.SystemColors.Window;
+            this.cookie.Location = new System.Drawing.Point(299, 75);
+            this.cookie.Name = "cookie";
+            this.cookie.Size = new System.Drawing.Size(189, 20);
+            this.cookie.TabIndex = 10;
+            // 
+            // maximum
+            // 
+            this.maximum.BackColor = System.Drawing.SystemColors.WindowText;
+            this.maximum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.maximum.ForeColor = System.Drawing.SystemColors.Window;
+            this.maximum.Location = new System.Drawing.Point(299, 117);
+            this.maximum.MaxLength = 6;
+            this.maximum.Name = "maximum";
+            this.maximum.Size = new System.Drawing.Size(189, 20);
+            this.maximum.TabIndex = 12;
+            // 
+            // bloodpoint
+            // 
+            this.bloodpoint.AutoSize = true;
+            this.bloodpoint.BackColor = System.Drawing.Color.Transparent;
+            this.bloodpoint.Location = new System.Drawing.Point(12, 192);
+            this.bloodpoint.Name = "bloodpoint";
+            this.bloodpoint.Size = new System.Drawing.Size(41, 13);
+            this.bloodpoint.TabIndex = 13;
+            this.bloodpoint.Text = "Added:";
+            // 
+            // allbloodpoint
+            // 
+            this.allbloodpoint.AutoSize = true;
+            this.allbloodpoint.BackColor = System.Drawing.Color.Transparent;
+            this.allbloodpoint.Location = new System.Drawing.Point(59, 192);
+            this.allbloodpoint.Name = "allbloodpoint";
+            this.allbloodpoint.Size = new System.Drawing.Size(22, 13);
+            this.allbloodpoint.TabIndex = 14;
+            this.allbloodpoint.Text = ". . .";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "thanks to Black";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(500, 222);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.allbloodpoint);
+            this.Controls.Add(this.bloodpoint);
+            this.Controls.Add(this.maximum);
+            this.Controls.Add(this.cookie);
+            this.Controls.Add(this.inject);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.Window;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button inject;
+        private System.Windows.Forms.TextBox cookie;
+        private System.Windows.Forms.TextBox maximum;
+        private System.Windows.Forms.Label bloodpoint;
+        private System.Windows.Forms.Label allbloodpoint;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ bloodslasher/bloodslasher/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Management;
+using System.Windows.Forms;
+using RestSharp;
+
+namespace bloodslasher
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string request(int balance, string type)
+        {
+            string json = "{" +
+                    "\"data\":{" + 
+                        "\"rewardType\": \"" + type + "\"," +
+                        "\"walletToGrant\": {" +
+                            "\"balance\":" +
+                            balance + 
+                        ",\"currency\": \"Bloodpoints\"}}}";
+
+            var client = new RestClient();
+            var bhvr_request = new RestRequest()
+            {
+                Resource = "https://steam.live.bhvrdbd.com/api/v1/extensions/rewards/grantCurrency/"
+            };
+            bhvr_request.AddHeader("Accept-Encoding", "deflate, gzip");
+            bhvr_request.AddHeader("Accept", "*/*");
+            bhvr_request.AddHeader("Content-Type", "application/json");
+            client.UserAgent = "DeadByDaylight/++DeadByDaylight+Live-CL-321933 Windows/10.0.19041.1.768.64bit";
+            bhvr_request.AddCookie("bhvrSession", cookie.Text);
+            bhvr_request.AddJsonBody(json);
+            IRestResponse response = client.Post(bhvr_request);
+            Console.WriteLine("\nRequest:\n" + json);
+            Console.WriteLine("\nResponse:\n" + response.Content);
+            return (response.Content);
+        }
+
+        private int check()
+        {
+            if (cookie.Text.Length > 0)
+            {
+                if (maximum.Text.Length > 0)
+                {
+
+                } else
+                {
+                    MessageBox.Show("Number of bloodpoint you want");
+                    return (-1);
+                }
+            } else
+            {
+                MessageBox.Show("Enter your bhvr cookie");
+                return (-1);
+            }
+            return (0);
+        }
+
+        private int goal(int balance)
+        {
+            int limit = 999999;
+            int max = Int32.Parse(maximum.Text);
+            Console.WriteLine("balance: " + balance);
+            Console.WriteLine("max:     " + max);
+            Console.WriteLine("limit:   " + limit);
+
+            if (balance > limit || balance > max)
+            {
+                return (1);
+            }
+            else
+            {
+                return (0);
+            }
+        }
+
+        private int counter(string response)
+        {
+            if (response.Contains("Reached the maximum number of grants"))
+            {
+                MessageBox.Show("You can't inject anymore, you need to wait. You reached the maximum number of grants (100) per hour");
+                return (-1);
+            } else if (response.Contains("Invalid input: The balance is exceeding the maximum value"))
+            {
+                MessageBox.Show("The balance is at the maximum value");
+                return (-1);
+            }
+            int value = Int32.Parse(response.Split(':')[2].Split('}')[0]);
+            return (value);
+        }
+
+        private int increase()
+        {
+            int[] array = { 10000, 12000, 15000 };
+            return (array[rnd.Next(0, array.Length)]);
+        }
+
+        private void resume(int balance)
+        {
+            if (balance > -1)
+            {
+                Console.WriteLine("update");
+                allbloodpoint.Text = balance.ToString();
+                allbloodpoint.Refresh();
+                Console.WriteLine("update: " + allbloodpoint.Text);
+            }
+        }
+
+        private void inject_Click(object sender, EventArgs e)
+        {
+            bool limit = false;
+            int total = 0;
+
+            if (check() == 0)
+            {
+                inject.BackColor = Color.Gray;
+                inject.ForeColor = Color.Gray;
+                inject.Enabled = false;
+
+                if (total != -1)
+                {
+                    allbloodpoint.Text = total.ToString();
+                    for (int i = 0; i < 100; i++)
+                    {
+                        total += increase();
+                        if (goal(total) == 0)
+                        {
+                            if (i < 4)
+                            {
+                                if (counter(request(150000, "shrineReward")) == -1)
+                                {
+                                    limit = true;
+                                    break;
+                                }
+                            } else
+                            {
+                                if (counter(request(total, "Story")) == -1)
+                                {
+                                    limit = true;
+                                    break;
+                                }
+                            }
+                        } else
+                        {
+                            limit = true;
+                            break;
+                        }
+                        resume(total);
+                    }
+                    if (limit == false)
+                    {
+                        resume(total);
+                        MessageBox.Show("Bloodpoints injected");
+                        limit = false;
+                    }
+                }
+                inject.ForeColor = Color.White;
+                inject.BackColor = Color.Transparent;
+                inject.Enabled = true;
+            }
+        }
+    }
+}
