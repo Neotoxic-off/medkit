c0a0825bd7ce5f15a2347e68165a47af2e372772
N e o <paul.gardien@epitech.eu>
2020-08-19 02:04:42 +0200

    Add files via upload

---
+++ Auricslasher/Auricslasher/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Auricslasher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.repeat = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.repeat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "made by neo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label5.Location = new System.Drawing.Point(126, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "hang1ng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(13, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Thanks to my patron:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.groupBox1.Location = new System.Drawing.Point(41, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(179, 82);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "STEP 1:  Cookie";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(16, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Cookie from Clipboard";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.repeat);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.groupBox2.Location = new System.Drawing.Point(283, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(179, 128);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "STEP 2:  order id";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gray;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button2.Location = new System.Drawing.Point(16, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Order id from Clipboard";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(13, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Status:";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.BackColor = System.Drawing.Color.Transparent;
+            this.status.ForeColor = System.Drawing.Color.Yellow;
+            this.status.Location = new System.Drawing.Point(54, 202);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(61, 13);
+            this.status.TabIndex = 16;
+            this.status.Text = "Initialisating";
+            // 
+            // repeat
+            // 
+            this.repeat.BackColor = System.Drawing.SystemColors.WindowText;
+            this.repeat.ForeColor = System.Drawing.Color.Transparent;
+            this.repeat.Location = new System.Drawing.Point(104, 88);
+            this.repeat.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.repeat.Name = "repeat";
+            this.repeat.Size = new System.Drawing.Size(55, 20);
+            this.repeat.TabIndex = 17;
+            this.repeat.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Repeat:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(500, 251);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auric Slasher v1.0";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.repeat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.NumericUpDown repeat;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Auricslasher/Auricslasher/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Auricslasher
+{
+    public partial class Form1 : Form
+    {
+        string bhvr_cookie = null;
+        string order_id = null;
+        public Form1()
+        {
+            InitializeComponent();
+            Logs("Ready", Color.LimeGreen);
+        }
+
+        public void Logs(string message, System.Drawing.Color color)
+        {
+            status.ForeColor = color;
+            status.Text = message;
+            status.Refresh();
+        }
+        private string validateIapPurchase()
+        {
+            string json = "{\"data\":{\"bundleId\":\"cellsPack1\",\"platform\":\"steam\",\"validationOptions\":{\"orderId\":\"" + order_id + "\"}}}";
+            var client = new RestClient();
+            var bhvr_request = new RestRequest()
+            {
+                Resource = "https://steam.live.bhvrdbd.com/api/v1/extensions/store/validateIapPurchase"
+            };
+
+            bhvr_request.AddHeader("Accept-Encoding", "deflate, gzip");
+            bhvr_request.AddHeader("Accept", "*/*");
+            bhvr_request.AddHeader("Content-Type", "application/json");
+            client.UserAgent = "DeadByDaylight/++DeadByDaylight+Live-CL-321933 Windows/10.0.19041.1.768.64bit";
+            bhvr_request.AddCookie("bhvrSession", bhvr_cookie);
+            bhvr_request.AddJsonBody(json);
+            IRestResponse response = client.Post(bhvr_request);
+            Console.WriteLine("\nsteamFinalizeTransaction Request:\n" + json);
+            Console.WriteLine("\nsteamFinalizeTransaction Response:\n" + response.Content);
+            return (response.Content);
+        }
+
+        private string consume()
+        {
+            string json = "{\"objectId\":\"cellsPack1\",\"quantity\":1}";
+            var client = new RestClient();
+            var bhvr_request = new RestRequest()
+            {
+                Resource = "https://steam.live.bhvrdbd.com/api/v1/inventories/consume/v2"
+            };
+
+            bhvr_request.AddHeader("Accept-Encoding", "deflate, gzip");
+            bhvr_request.AddHeader("Accept", "*/*");
+            bhvr_request.AddHeader("Content-Type", "application/json");
+            client.UserAgent = "DeadByDaylight/++DeadByDaylight+Live-CL-321933 Windows/10.0.19041.1.768.64bit";
+            bhvr_request.AddCookie("bhvrSession", bhvr_cookie);
+            bhvr_request.AddJsonBody(json);
+            IRestResponse response = client.Post(bhvr_request);
+            Console.WriteLine("\nvalidateIapPurchase Request:\n" + json);
+            Console.WriteLine("\nvalidateIapPurchase Response:\n" + response.Content);
+            return (response.Content);
+        }
+
+        private string steamFinalizeTransaction()
+        {
+            string json = "{\"data\":{\"isCancelled\": false,\"orderId\":\"" + order_id + "\"}}";
+            var client = new RestClient();
+            var bhvr_request = new RestRequest()
+            {
+                Resource = "https://steam.live.bhvrdbd.com/api/v1/extensions/store/steamFinalizeTransaction"
+            };
+
+            bhvr_request.AddHeader("Accept-Encoding", "deflate, gzip");
+            bhvr_request.AddHeader("Accept", "*/*");
+            bhvr_request.AddHeader("Content-Type", "application/json");
+            client.UserAgent = "DeadByDaylight/++DeadByDaylight+Live-CL-321933 Windows/10.0.19041.1.768.64bit";
+            bhvr_request.AddCookie("bhvrSession", bhvr_cookie);
+            bhvr_request.AddJsonBody(json);
+            IRestResponse response = client.Post(bhvr_request);
+            Console.WriteLine("\nvalidateIapPurchase Request:\n" + json);
+            Console.WriteLine("\nvalidateIapPurchase Response:\n" + response.Content);
+            return (response.Content);
+        }
+
+        private void launch()
+        {
+            for (int i = 1; i <= repeat.Value; i++)
+            {
+                steamFinalizeTransaction();
+                validateIapPurchase();
+                consume();
+                Logs("Transaction number: " + i, Color.Red);
+            }
+            MessageBox.Show("All completed");
+            unlock();
+        }
+        /*
+         * orderId=1597057995323
+         * orderId=1597058001860
+         * orderId=1597058007903
+         * orderId=1597058115006
+         * orderId=1597058122533
+         * base: 1597057
+        */
+        private void unlock()
+        {
+            button2.Enabled = true;
+            button2.ForeColor = Color.White;
+            button2.BackColor = Color.Transparent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                bhvr_cookie = Clipboard.GetText(TextDataFormat.Text);
+                button1.Enabled = false;
+                button1.ForeColor = Color.Black;
+                button1.BackColor = Color.Gray;
+                unlock();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                order_id = Clipboard.GetText(TextDataFormat.Text);
+                button2.Enabled = false;
+                button2.ForeColor = Color.Black;
+                button2.BackColor = Color.Gray;
+                launch();
+            }
+        }
+    }
+}
