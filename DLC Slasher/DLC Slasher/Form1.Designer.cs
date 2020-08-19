9a74234da5215f9b3a9c6df75001490c37de2417
N e o <paul.gardien@epitech.eu>
2020-08-19 02:06:53 +0200

    Add files via upload

---
+++ DLC Slasher/DLC Slasher/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace DLC_Slasher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.version = new System.Windows.Forms.ComboBox();
+            this.unlock = new System.Windows.Forms.Button();
+            this.cells = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cookie = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(460, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "v1.8";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "made by neo";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.version);
+            this.groupBox1.Controls.Add(this.unlock);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.groupBox1.Location = new System.Drawing.Point(278, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(175, 120);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Step 2: Unlock";
+            // 
+            // version
+            // 
+            this.version.BackColor = System.Drawing.SystemColors.WindowText;
+            this.version.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.version.ForeColor = System.Drawing.SystemColors.Window;
+            this.version.FormattingEnabled = true;
+            this.version.Location = new System.Drawing.Point(18, 34);
+            this.version.Name = "version";
+            this.version.Size = new System.Drawing.Size(141, 21);
+            this.version.TabIndex = 3;
+            // 
+            // unlock
+            // 
+            this.unlock.BackColor = System.Drawing.Color.Transparent;
+            this.unlock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.unlock.ForeColor = System.Drawing.SystemColors.Window;
+            this.unlock.Location = new System.Drawing.Point(18, 77);
+            this.unlock.Name = "unlock";
+            this.unlock.Size = new System.Drawing.Size(141, 23);
+            this.unlock.TabIndex = 2;
+            this.unlock.Text = "Unlock DLC";
+            this.unlock.UseVisualStyleBackColor = false;
+            this.unlock.Click += new System.EventHandler(this.unlock_Click);
+            // 
+            // cells
+            // 
+            this.cells.AutoSize = true;
+            this.cells.BackColor = System.Drawing.Color.Transparent;
+            this.cells.ForeColor = System.Drawing.Color.Yellow;
+            this.cells.Location = new System.Drawing.Point(79, 42);
+            this.cells.Name = "cells";
+            this.cells.Size = new System.Drawing.Size(51, 13);
+            this.cells.TabIndex = 17;
+            this.cells.Text = "unknown";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(14, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Auric Cells:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.cells);
+            this.groupBox2.Controls.Add(this.cookie);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.groupBox2.Location = new System.Drawing.Point(45, 43);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(175, 120);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Step 1: Cookie";
+            // 
+            // cookie
+            // 
+            this.cookie.BackColor = System.Drawing.Color.Transparent;
+            this.cookie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cookie.ForeColor = System.Drawing.SystemColors.Window;
+            this.cookie.Location = new System.Drawing.Point(16, 77);
+            this.cookie.Name = "cookie";
+            this.cookie.Size = new System.Drawing.Size(141, 23);
+            this.cookie.TabIndex = 1;
+            this.cookie.Text = "Cookie from clipoard";
+            this.cookie.UseVisualStyleBackColor = false;
+            this.cookie.Click += new System.EventHandler(this.cookie_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(12, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Status:";
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.BackColor = System.Drawing.Color.Transparent;
+            this.status.ForeColor = System.Drawing.Color.Yellow;
+            this.status.Location = new System.Drawing.Point(58, 190);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(60, 13);
+            this.status.TabIndex = 15;
+            this.status.Text = "initialisation";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(500, 212);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DLC Slasher";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button cookie;
+        private System.Windows.Forms.Button unlock;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Label cells;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox version;
+    }
+}
+
---
+++ DLC Slasher/DLC Slasher/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using RestSharp;
+using Newtonsoft.Json;
+using System.IO;
+using System.Diagnostics;
+
+namespace DLC_Slasher
+{
+    public partial class Form1 : Form
+    {
+        string bhvr_cookie = null;
+
+        public class Rootobject
+        {
+            public List[] list { get; set; }
+        }
+
+        public class List
+        {
+            public int balance { get; set; }
+            public string currency { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            init_files();
+            update(unlock);
+            Logs("Ready", Color.LimeGreen);
+        }
+
+        private int errors(string response)
+        {
+            if (response.Contains("invalid token") || response.Length == 0)
+            {
+                boxer("Your cookie is incorrect", "Incorrect cookie", MessageBoxIcon.Error);
+                return (1);
+            }
+            return (0);
+        }
+
+        private int wallet(string cookie)
+        {
+            var response = request("https://steam.live.bhvrdbd.com/api/v1/wallet/currencies", null, cookie, 1);
+            Rootobject weps = JsonConvert.DeserializeObject<Rootobject>(response);
+
+            if (errors(response) == 0)
+            {
+                if (weps != null)
+                {
+                    foreach (List item in weps.list)
+                    {
+                        if (item.currency == "Cells")
+                        {
+                            cells.Text = item.balance.ToString();
+                            cells.Refresh();
+                            return (item.balance);
+                        }
+                    }
+                }
+                else
+                {
+                    boxer("Your cookie is incorrect", "Incorrect cookie", MessageBoxIcon.Error);
+                    return (-1);
+                }
+            }
+            return (-1);
+        }
+
+        private void update(Button button)
+        {
+            if (button.Enabled == true)
+            {
+                button.BackColor = Color.Gray;
+                button.ForeColor = Color.Gray;
+                button.Enabled = false;
+            }
+            else
+            {
+                button.ForeColor = Color.White;
+                button.BackColor = Color.Transparent;
+                button.Enabled = true;
+            }
+        }
+
+        private string request(string url, string json, string cookie, int method)
+        {
+            IRestResponse response;
+            var client = new RestClient();
+            var bhvr_request = new RestRequest()
+            {
+                Resource = url
+            };
+            bhvr_request.AddHeader("Accept-Encoding", "deflate, gzip");
+            bhvr_request.AddHeader("Accept", "*/*");
+            bhvr_request.AddHeader("Content-Type", "application/json");
+            client.UserAgent = "DeadByDaylight/++DeadByDaylight+Live-CL-321933 Windows/10.0.19041.1.768.64bit";
+            bhvr_request.AddCookie("bhvrSession", cookie);
+            if (json != null)
+                bhvr_request.AddJsonBody(json);
+            if (method == 0)
+                response = client.Post(bhvr_request);
+            else
+                response = client.Get(bhvr_request);
+            Console.WriteLine("\nResponse:\n" + response.Content);
+            return (response.Content);
+        }
+
+        public void Logs(string message, System.Drawing.Color color)
+        {
+            status.ForeColor = color;
+            status.Text = message;
+            status.Refresh();
+        }
+
+        private void boxer(string message, string title, System.Windows.Forms.MessageBoxIcon type)
+        {
+            const MessageBoxButtons buttons = MessageBoxButtons.OK;
+            MessageBox.Show(message, title, buttons, type);
+        }
+
+        private int check_path(string path)
+        {
+            if (File.Exists(path))
+                return (1);
+            return (0);
+        }
+
+        private string[] init_files()
+        {
+            string[] files = Directory.GetFiles(".", "*.dlc");
+
+            if (files.Length > 0)
+            {
+                for (int i = 0; i < files.Length; i++)
+                {
+                    version.Items.Add(files[i].Split('\\')[1]);
+                    version.SelectedIndex = 0;
+                    version.Refresh();
+                }
+            }
+            else
+            {
+                Logs("No dlc version found", Color.Red);
+            }
+            return (files);
+        }
+
+        private string[] load_dlc()
+        {
+            if (check_path(version.GetItemText(version.SelectedItem)) == 1)
+            {
+                Logs("Dlc version found", Color.LimeGreen);
+                string text = File.ReadAllText(version.GetItemText(version.SelectedItem));
+                string[] tokens = text.Split(' ');
+                string[] data = new string[tokens.Length];
+
+                Logs("Reading Dlc availables", Color.Yellow);
+                for (int i = 0; i < tokens.Length; i++)
+                {
+                    data[i] = tokens[i];
+                }
+                return (data);
+            }
+            else
+            {
+                Logs("Dlc version not found", Color.Red);
+                return (null);
+            }
+        }
+
+        private void unlock_Click(object sender, EventArgs e)
+        {
+            if (wallet(bhvr_cookie) < 500)
+            {
+                boxer("To unlock DLC you need to have at least [500] Auric Cells on your account", "Requirements", MessageBoxIcon.Error);
+            } else
+            {
+                update(unlock);
+                string[] characters = load_dlc();
+                if (characters.Length > 0)
+                {
+                    string json = "{\"currencyType\":\"Cells\",\"wantedQuantity\":1}";
+
+                    for (int i = 0; i < characters.Length; i++)
+                    {
+                        request("https://steam.live.bhvrdbd.com/api/v1/purchases/consume/" + characters[i], json, bhvr_cookie, 0);
+                        Logs(characters[i], Color.Purple);
+                    }
+                    Logs("DLC Unlocked", Color.LimeGreen);
+                } else
+                {
+                    Logs("No Dlc found", Color.Red);
+                }
+            }
+        }
+
+        private void cookie_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                bhvr_cookie = Clipboard.GetText(TextDataFormat.Text);
+                if (wallet(bhvr_cookie) != -1)
+                {
+                    update(unlock);
+                    update(cookie);
+                }
+            }
+        }
+    }
+}
